76b72260222d9514b904fff10e96210c938155ef
trevorjones1989 <trevorjones1989@users.noreply.github.com>
2026-05-28 15:49:57 -0700

    Redirect www.runforge.ca to runforge.ca

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.runforge.ca" }],
+        destination: "https://runforge.ca/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
